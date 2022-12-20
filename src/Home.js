326b9f29d0698037e08e79d248fad1b8cbeeb905
Nandy Bâ <nandyba@yahoo.fr>
2022-12-20 03:09:43 +0100

    Website - Add name and center social media links

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import { FaTwitter, FaLinkedin } from "react-icons/fa";
 function Home() {
   return (
     <div>
+      <h2>Nandy BÂ</h2>
       <h1>DeFi Specialist | Advisor | Blockchain builder</h1>
       <h2>
         <a href='https://twitter.com/Nandy_ba'><FaTwitter/></a>
