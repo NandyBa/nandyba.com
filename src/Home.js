4eb1fb8a4a230f3b399afd5ab8946ba28958732f
Nandy Bâ <nandyba@yahoo.fr>
2022-12-20 01:57:44 +0100

    Website - Basic website

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import { FaTwitter, FaLinkedin } from "react-icons/fa";
+
+
+function Home() {
+  return (
+    <div>
+      <h1>DeFi Specialist | Advisor | Blockchain builder</h1>
+      <h2>
+        <a href='https://twitter.com/Nandy_ba'><FaTwitter/></a>
+        <a href='https://linkedin.com/in/nandyba'><FaLinkedin/></a>
+      </h2>
+      
+    </div>
+  )
+}
+
+export default Home
