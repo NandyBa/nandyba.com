4b779ca0bf9d6f1f13d43ea401d79e5cbab73b7f
Nandy Bâ <nandyba@yahoo.fr>
2023-01-01 20:12:52 +0100

    LinkedIn Formation change iframe properties

--- src/page/Formations/LinkedIn.js
+++ src/page/Formations/LinkedIn.js
@@ -7,7 +7,7 @@ const FormationLinkedIn = () => {
 
             <div>
                 <div className="video">
-                     <iframe width='100%'src="https://youtu.be/embed/aag6QmmhXxM" title="Youtube - Formation LinkedIn"/>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/aag6QmmhXxM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
                
                 <div className='video-parts'>
