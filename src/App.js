6126a14b18e0ee9f32f12ed9f2acd263ffa0c9a2
Nandy Bâ <nandyba@yahoo.fr>
2025-01-12 20:28:52 +0100

    feat: add analytics (#3)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './page/Home';
-import Formations from './page/Formations/Formations';
-import FormationLinkedIn from './page/Formations/LinkedIn';
-import FormationGitHub from './page/Formations/GitHub';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./page/Home";
+import Formations from "./page/Formations/Formations";
+import FormationLinkedIn from "./page/Formations/LinkedIn";
+import FormationGitHub from "./page/Formations/GitHub";
+import { Analytics } from "@vercel/analytics/react";
+import "./App.css";
 
 const App = () => {
-
-  return(
+  return (
     <BrowserRouter>
       <Routes>
         <Route path="">
@@ -16,14 +16,15 @@ const App = () => {
             <Route path="" element={<Home />}/>
             <Route path="pre-registration" element={<PreRegistration/>} />
           </Route> */}
-          <Route path="" element={<Home />}/>
+          <Route path="" element={<Home />} />
           <Route path="formations" element={<Formations />} />
           <Route path="formations/LinkedIn" element={<FormationLinkedIn />} />
           <Route path="formations/GitHub/pdf" element={<FormationGitHub />} />
         </Route>
       </Routes>
+      <Analytics />
     </BrowserRouter>
   );
-}
+};
 
 export default App;
